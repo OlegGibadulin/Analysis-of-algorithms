f37f5c79f6ec8c3784bb0fd862fbcdcab70abb14
OlegGibadulin <O.Gibadulin@yandex.ru>
2019-11-04 19:17:06 +0300

    Add creating and displaying graph of distance

--- lab_06_ACO/lab_06_ACO/main.cpp
+++ lab_06_ACO/lab_06_ACO/main.cpp
@@ -11,13 +11,39 @@
 
 using namespace std;
 
+
 void getRandomGraph(vector< vector <int>>& graph, const size_t graphSize) {
-    ;
+    for (int i = 0; i < graphSize; ++i) {
+        vector<int> line;
+        for (int j = 0; j < graphSize; ++j) {
+            if (i == j) {
+                line.push_back(0);
+            }
+            else {
+                line.push_back(rand() % 9 + 1);
+            }
+        }
+        graph.push_back(line);
+    }
 }
 
 
 void showGraph(const vector< vector <int>>& graph, const size_t graphSize) {
-    ;
+    if (graph.size() == 0)
+    {
+        cout << "This graph is empty" << endl;
+    }
+    cout << endl;
+    
+    for (auto &line : graph)
+    {
+        for (auto &elem : line)
+        {
+            cout << elem << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
 
@@ -32,16 +58,12 @@ size_t ACO(const vector< vector <int>>& graph) {
 
 
 int main(int argc, const char * argv[]) {
-    size_t repeatCount = 10;
-    
-    while (true)
-    {
+    while (true) {
         int key;
         cout << "Keys:\n1 - Algorithm work\n2 - Time\n0 - Exit\n\n" << "Input key: " << endl;
         cin >> key;
         
-        if (key == 1)
-        {
+        if (key == 1) {
             size_t start;
             size_t end;
             size_t pathLength;
@@ -52,17 +74,10 @@ int main(int argc, const char * argv[]) {
             cout << "Input graph size: ";
             cin >> graphSize;
             
-            if (graphSize < 0)
-            {
+            if (graphSize < 0) {
                 cout << "Error size";
                 return 1;
             }
-            
-            /*for (int i = 0; i < graphSize; ++i) {
-                int num;
-                cin >> num;
-                graph.push_back(num);
-            }*/
             
             getRandomGraph(graph, graphSize);
             showGraph(graph, graphSize);
@@ -70,16 +85,17 @@ int main(int argc, const char * argv[]) {
             start = __rdtsc();
             pathLength = bruteForce(graph);
             end = __rdtsc();
-            cout << "Brute force:" << pathLength << ", time: " << (end - start) << endl;
+            cout << "Brute force: " << pathLength << ", time: " << (end - start) << endl;
             
             start = __rdtsc();
             pathLength = ACO(graph);
             end = __rdtsc();
-            cout << "ACO" << pathLength << ", time: " << (end - start) << endl;
+            cout << "ACO: " << pathLength << ", time: " << (end - start) << endl;
             
             cout << endl;
         }
         else if (key == 2) {
+            size_t repeatCount = 10;
             for (int j = 1; j < 10; j += 1) {
                 vector< vector <int>> graph;
                 size_t graphSize = j;
@@ -98,8 +114,6 @@ int main(int argc, const char * argv[]) {
                 end = __rdtsc();
                 
                 cout << " time: " << (end - start) / repeatCount  << endl;
-                
-                cout << endl;
             }
         }
         else if (key == 0) {
