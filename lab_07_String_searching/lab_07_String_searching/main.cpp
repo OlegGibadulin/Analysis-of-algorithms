dee1050e176bb1188be99d11ee16246bcf29e4a1
OlegGibadulin <O.Gibadulin@yandex.ru>
2019-12-14 09:01:04 +0300

    feat: Add BM algorithm

--- lab_07_String_searching/lab_07_String_searching/main.cpp
+++ lab_07_String_searching/lab_07_String_searching/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -16,7 +17,7 @@ void getPrefixFunction(vector <int> &pf, const string &pattern) {
     pf[0] = 0;
     
     for (int i = 1, j = 0; i < pattern.size(); ++i) {
-        while (j > 0 && pattern[i] != pattern[j]) {
+        if (j > 0 && pattern[i] != pattern[j]) {
             j = pf[j - 1];
         }
         
@@ -29,14 +30,13 @@ void getPrefixFunction(vector <int> &pf, const string &pattern) {
 }
 
 vector <int> KMP(const string &s, const string &pattern) {
+    vector <int> mathesIndexes;
     vector <int> pf(pattern.size());
     
     getPrefixFunction(pf, pattern);
     
-    vector <int> mathesIndexes;
-    
     for (int i = 0, j = 0; i < s.size(); ++i) {
-        while (j > 0 && pattern[j] != s[i]) {
+        if (j > 0 && pattern[j] != s[i]) {
             j = pf[j - 1];
         }
         
@@ -52,11 +52,52 @@ vector <int> KMP(const string &s, const string &pattern) {
     return mathesIndexes;
 }
 
+void getSlide(map <char, int> &slide, const string &pattern) {
+    for (int i = 0; i < pattern.size(); ++i) {
+        slide[pattern[i]] = pattern.size() - i;
+    }
+}
+
+vector <int> BM(const string &s, const string &pattern) {
+    vector <int> mathesIndexes;
+    map <char, int> slide;
+    
+    getSlide(slide, pattern);
+    
+    int sLen = s.size();
+    int patternLen = pattern.size();
+    int sInd = 0;
+    
+    while (sInd <= sLen - patternLen) {
+        int patternInd = patternLen - 1;
+        
+        while (patternInd >= 0 && pattern[patternInd] == s[sInd + patternInd]) {
+            --patternInd;
+        }
+        
+        if (patternInd < 0) {
+            mathesIndexes.push_back(sInd);
+            sInd += patternLen;
+        }
+        else {
+            if (slide.count(pattern[patternInd]) == 0) {
+                sInd += patternLen;
+            }
+            else {
+                sInd += slide.find(pattern[patternInd])->second;
+            }
+        }
+    }
+    
+    return mathesIndexes;
+}
+
 int main(int argc, const char * argv[]) {
     string s1 = "asfasfashjk";
-    string s2 = "f";
+    string s2 = "asf";
     vector <int> mathesIndexes = KMP(s1, s2);
     
+    cout << "KMP" << endl;
     if (mathesIndexes.empty()) {
         cout << s1 << " does not contatain " << s2 << endl;
     }
@@ -67,5 +108,20 @@ int main(int argc, const char * argv[]) {
         }
         cout << endl;
     }
+    
+    mathesIndexes = BM(s1, s2);
+    
+    cout << "BM" << endl;
+    if (mathesIndexes.empty()) {
+        cout << s1 << " does not contatain " << s2 << endl;
+    }
+    else {
+        cout << s1 << " contatains " << s2 << " by index:";
+        for (int i = 0; i < mathesIndexes.size(); ++i) {
+            cout << " " << mathesIndexes[i];
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
